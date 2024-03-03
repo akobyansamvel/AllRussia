6b0420e93426c1c375dd45356501450c97fefcb5
A-v-polunin <polunin-sasha2003@mail.ru>
2024-03-03 17:18:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <MoscowScreen/>
+</template>
+
+<script>
+import MoscowScreen from './components/MoscowScreen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MoscowScreen
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MoscowScreen.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="main">
+      <div class="wrapper">
+        <div class="headding">
+          <img class="headding__logo" src="../assets/gerb.svg" alt="Герб">
+          <p class="headding__text">Москва</p>
+        </div>
+        <div class="box">
+          <p class="bow__text">Население:  12 330 126 человек</p>
+          <p class="bow__text">Площадь: 2,561 тыс.км²  </p>
+        </div>
+        <div class="box">
+
+        </div>
+        <div class="list">
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MoscowScreen',
+}
+</script>
+
+<style scoped>
+
+.box{
+  border-top: 1px black solid;
+}
+
+
+</style>
