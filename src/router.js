81c4917e7556793996bef2356456e201235f2ec3
A-v-polunin <polunin-sasha2003@mail.ru>
2024-03-04 14:19:34 +0300

    contact and footer

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/HomePage.vue';
+import Contact from './components/ContactPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     },
   ]
 });
