e1d24aab42299a03d0c39a13c6839889f7c3f87b
A-v-polunin <polunin-sasha2003@mail.ru>
2024-03-10 18:52:22 +0300

    navbar

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
         MAIN page
+        <div class="wrap">
+          <NavigationBar></NavigationBar>
+        </div>
     </div>
 </template>
 
 <script>
+import NavigationBar from "./NavigationBar.vue"
+
 export default {
   name: 'MainPage',
+  components: {
+    NavigationBar,
+  }
 }
 </script>
 
 <style>
-
+.wrap{
+  margin-left:100px ;
+}
 </style>
